2da3231182f95356c4944e5e5a1df7d9996207ad
sh0rk1n <155022624+sh0rk1n@users.noreply.github.com>
2024-05-08 20:11:52 +0300

    hero layout ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root px-20">
+      <div>
+        <header className="flex items-center justify-between  max-w-1760 pt-2.5 pb-48">
+
+          <img width={40} height={40} src="/img/logo.svg" alt="Logo"/>
+          <nav>
+            <ul className="flex">
+              <li>Главная</li>
+              <li>Курсы</li>
+              <li>Расписание</li>
+              <li>Преподаватели</li>
+              <li>Рассылка</li>
+              <li>Контакты</li>
+            </ul>
+          </nav>
+          <button className="">Зайти в кабинет</button>
+        </header>
+        <div>
+          <h1>Первый курс по компьютерной сборке</h1>
+          <div>
+            <span>18 дней</span>
+            <span>18 часов</span>
+            <span>18 минут</span>
+            <span>18 секунд</span>
+          </div>
+        </div>
+        <div>
+          <button>Заказать курс</button>
+          <div>
+            <p>Учеников всего: 200</p>
+            <p>Успешно закончили курс: 190</p>
+          </div>
+          <div>
+            <p>Заработано учениками: 400 000&#8381;</p>
+            <div></div>
+            <div>
+              <p>0</p>
+              <p>1 000 000&#8381;</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
