2886e3a526eb5c04d784a404b05678094fec8db3
sh0rk1n <155022624+sh0rk1n@users.noreply.github.com>
2024-05-09 00:41:00 +0300

    about, start, advantages components layout done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="wrapper max-2xl my-0 mx-auto text-base text-white">
-      <div className="hero ">
+    <div className="wrapper max-2xl my-0 mx-auto text-base text-white ">
+      <div className="hero">
         <header className="flex items-center justify-between  max-w-1760 pt-7 pb-48 px-20">
           <img width={40} height={40} src="/img/logo.svg" alt="Logo"/>
           <nav>
@@ -71,6 +71,73 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="about flex px-20 mb-48 ">
+        <img src="/img/pc.svg" alt=""/>
+        <div className="max-w-screen-sm ml-36 content-center">
+          <h2 className="text-5xl font-bold mb-11">Чем мы занимаемся?</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget neque, dignissim et feugiat elit augue in suspendisse egestas. Dictum vestibulum mi et sed nunc, orci fermentum vestibulum, morbi. Et neque, adipiscing sapien sem senectus praesent aenean consequat. Aenean facilisi turpis aliquet fringilla. Nunc sem felis sed interdum feugiat mattis elit, sollicitudin. Quam pharetra rhoncus risus, cursus id elementum aliquet. <br/> Nullam turpis arcu malesuada arcu interdum placerat nisi, lobortis. </p>
+        </div>
+      </div>
+      <div className="start px-20 mb-48">
+        <div className="flex items-center text-center">
+          <img width={70} height={45} src="/img/logo-start.svg" alt=""/>
+          <h2 className="text-5xl font-bold ml-5 ">Быстрый старт</h2>
+        </div>
+        <p className="max-w-md text-neutral-500 mt-10 ml-1 mb-20 ">Больше 90% учеников прошли полный курс и смогли собрать свой первый компьютер</p>
+        <div className="text-4xl font-bold">
+          <span>100%</span>
+          <span>75%</span>
+          <span>50%</span>
+          <span>Итог</span>
+          <div className="flex items-center mt-5">
+            <div className="w-80 h-20 bg-gradient-to-br from-purple-400 to-indigo-600"></div>
+            <div className="w-80 h-14 bg-gradient-to-br from-purple-400 to-indigo-600"></div>
+            <div className="w-80 h-8 bg-gradient-to-br from-purple-400 to-indigo-600"></div>
+            <div className="w-80 h-2.5 bg-gradient-to-br from-purple-400 to-indigo-600"></div>
+          </div>
+          <div className="flex font-normal mt-5 items-center">
+            <p className="text-sm text-neutral-500 max-w-72 mr-9">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget
+              velit quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat
+              dui.</p>
+            <p className="text-sm text-neutral-500 max-w-72 mr-9 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget
+              velit quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat
+              dui.</p>
+            <p className="text-sm text-neutral-500 max-w-72 mr-9 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget
+              velit quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat
+              dui.</p>
+            <p className="text-sm text-neutral-500 max-w-72 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget
+              velit quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat
+              dui.</p>
+          </div>
+
+        </div>
+      </div>
+      <div className="advantages  px-20 ">
+        <h2 className="text-5xl font-bold mb-28 text-center">Получите профессию прямо сейчас</h2>
+        <div className="flex text-center items-center">
+          <div className="flex flex-col items-center mr-20">
+            <img width={100} height={100} className="mb-11" src="/img/icon-1.svg" alt=""/>
+            <h3 className="text-2xl font-medium mb-7">Только практические <br/> навыки в работе</h3>
+            <p className="text-neutral-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget velit
+              quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus.</p>
+          </div>
+
+          <div className="flex flex-col items-center mr-20">
+            <img width={100} height={100} className="mb-11" src="/img/icon-2.svg" alt=""/>
+            <h3 className="text-2xl font-medium mb-7">Работа на самом <br/>современном оборудовании</h3>
+            <p className="text-neutral-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget velit
+              quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus.</p>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img width={100} height={100} className="mb-11" src="/img/icon-3.svg" alt=""/>
+            <h3 className="text-2xl font-medium mb-7">Сертификация <br/> по окончании обучения</h3>
+            <p className="text-neutral-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Netus eget velit
+              quisque accumsan amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus.</p>
+          </div>
+        </div>
+      </div>
+      <div className="partners"></div>
     </div>
   );
 }
